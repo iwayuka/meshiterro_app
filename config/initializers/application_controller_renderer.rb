c212ac138963c821e4d6c1983b25620acf12be96
EC2 Default User <iwabuchi.yuka07@gmail.com>
2021-10-23 05:30:46 +0000

    [Add] 4-3 index

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '07a18cdee37ca6f7db26b213b5df992a1dbba63709e250932f868d85280623f27b031bcee7414f34666e4d4bc737600df73560c0536563eb26c413ad3a290ac7'
+
